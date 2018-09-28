9ead103c6975f4b80e083423bd09d7604679ddba
ryam <ryambudha@gmail.com>
2018-09-28 12:44:18 +0530

    heroku

---
+++ jobBoard/config/passport.js
@@ -0,0 +1,18 @@
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    //mongoose = require('mongoose'),
+    User = require('../model/users/user');
+
+passport.use(new LocalStrategy({
+    usernameField: 'user[username]',
+    passwordField: 'user[password]'
+}, function(username, password, done){
+    console.log("passport reached")
+    User.findOne({username: username}).then(function(user){
+        if (!user || !user.comparePassword(password)) {
+            return done(null, false, {errors: {'email or password': 'is invalid'}});
+            }
+        return done(null, user);
+        }).catch(done);
+}));
+// dkfj
